d9dd8619c2fc841086672aabb067de130c64df9b
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-19 13:50:08 -0600

    Adds validations to Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,9 @@
 class Customer < ApplicationRecord
   has_many :subscriptions
+
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email,
+                        :address
+
 end
