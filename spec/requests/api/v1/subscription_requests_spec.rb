8ff19de3966936b5e6230a75faa7dd54f762ad91
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-19 14:52:40 -0600

    Create response test for Subscriptions POST request

---
+++ spec/requests/api/v1/subscription_requests_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Subscription Requests API', type: :request do
+  describe 'POST /api/v1/subscription_requests' do
+    let!(:customer) { create(:customer) }
+    let!(:tea) { create(:tea) }
+
+    describe 'Happy Path' do
+      it 'creates a subscription' do
+        subscription_params = {
+          title: Faker::Sports::Basketball.team,
+          price: Faker::Number.decimal(l_digits: 2),
+          status: 1,
+          frequency: Faker::Number.between(from: 1, to: 3),
+          tea_id: tea.id,
+          customer_id: customer.id
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        post '/api/v1/subscriptions', headers: headers, params: JSON.generate(subscription_params)
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+      end
+    end
+  end
+end
