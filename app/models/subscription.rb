09f8a885061481ef4c95340a9942dedc2f3bcccd
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-19 13:56:10 -0600

    Adds validations and status enum for Subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :tea
   belongs_to :customer
+
+  validates_presence_of :title,
+                        :price,
+                        :status,
+                        :frequency
+
+  enum status: { inactive: 0, active: 1 }
 end
