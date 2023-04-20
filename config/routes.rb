bd78c970e7f2a3ec361f426cdb7abd083565364d
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-20 12:44:56 -0600

    Add subscriptions index resource within customers resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subscriptions, only: [:create, :update]
+      resources :customers, only: [:create] do
+        scope module: :customers do
+          resources :subscriptions, only: [:index]
+        end
+      end
     end
   end
 end
