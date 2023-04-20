d705a2e59529c406da3fe7da27bf574607df23a6
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-20 10:16:59 -0600

    Fix factory enum range for frequency

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     title { Faker::Sports::Basketball.team }
     price { Faker::Number.decimal(l_digits: 2) }
     status { Faker::Number.between(from: 0, to: 1) }
-    frequency { Faker::Number.between(from: 1, to: 3) }
+    frequency { Faker::Number.between(from: 0, to: 2) }
     tea { Tea.all.shuffle.sample }
     customer { Customer.all.shuffle.sample }
   end
