75e9bac463d7364fa4f3c2063e79f81f96fe044c
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-20 23:17:33 -0600

    Create response test for Tea POST request

---
+++ spec/requests/api/v1/teas/tea_requests_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Tea Requests API', type: :request do
+  describe 'POST Tea Requests' do
+    it 'creates a tea' do
+      tea_params = {
+        title: Faker::Tea.variety,
+        description: Faker::Coffee.notes,
+        temperature: Faker::Number.between(from: 100.0, to: 150.0),
+        brew_time: Faker::Number.number(digits: 2)
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/teas', headers: headers, params: JSON.generate(tea_params)
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+    end
+  end
+end
