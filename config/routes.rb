7b8194b86d23af4e286891cd0bed81f48a018d76
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-20 23:18:27 -0600

    Add teas create resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :teas, only: [:create]
       resources :subscriptions, only: [:create, :update]
       resources :customers, only: [:create] do
         scope module: :customers do
