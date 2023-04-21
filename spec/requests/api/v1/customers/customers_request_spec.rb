1327dc0bcb593ed9dfd2df1e3849be649db1f1ba
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-04-20 23:11:52 -0600

    Add test expectations to Customer POST requests

--- spec/requests/api/v1/customers/customers_request_spec.rb
+++ spec/requests/api/v1/customers/customers_request_spec.rb
@@ -11,11 +11,23 @@ RSpec.describe 'Customer Requests API', type: :request do
       }
 
       headers = { 'CONTENT_TYPE' => 'application/json' }
-
       post '/api/v1/customers', headers: headers, params: JSON.generate(customer_params)
+      response_body = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
+
+      expect(response_body[:data][:attributes][:first_name]).to eq(customer_params[:first_name])
+      expect(response_body[:data][:attributes][:last_name]).to eq(customer_params[:last_name])
+      expect(response_body[:data][:attributes][:email]).to eq(customer_params[:email])
+      expect(response_body[:data][:attributes][:address]).to eq(customer_params[:address])
+
+      customer = Customer.last
+
+      expect(customer.first_name).to eq(customer_params[:first_name])
+      expect(customer.last_name).to eq(customer_params[:last_name])
+      expect(customer.email).to eq(customer_params[:email])
+      expect(customer.address).to eq(customer_params[:address])
     end
   end
 end
